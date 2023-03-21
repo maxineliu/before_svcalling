7caf4287c3f67a4339f19c86ded4de3e1b5a806c
maxineliu <rliu13@uoguelph.ca>
2023-03-21 16:44:41 -0400

    Update align_minimap.sh
    fixed bugs

--- align_minimap.sh
+++ align_minimap.sh
@@ -6,16 +6,18 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --mail-type=All #Valid type values are NONE, BEGIN, END, FAIL, REQUEUE, ALL (equivalent to BEGIN, END, FAIL, INVALID_DEPEND, REQUEUE, and STAGE_OUT), INVALID_DEPEND (dependency never satisfied), STAGE_OUT (burst buffer stage out and teardown completed), TIME_LIMIT, TIME_LIMIT_90 (reached 90 percent of time limit), TIME_LIMIT_80 (reached 80 percent of time limit), TIME_LIMIT_50 (reached 50 percent of time limit) and ARRAY_TASKS (send emails for each array task).
 #SBATCH --mail-user=rliu13@uoguelph.ca
-#SBATCH --array=2-12 #e.g.--array=0,6,16-32, --array=0-15:4 (equivalent to --array=0,4,8,12), --array=0-15%4 (limit number of simultaneously running jobs) 
-set -xv
+#SBATCH --array=1-12 #e.g.--array=0,6,16-32, --array=0-15:4 (equivalent to --array=0,4,8,12), --array=0-15%4 (limit number of simultaneously running jobs) 
+#set -xv
 
 start_time=`date --date='0 days ago' "+%Y-%m-%d %H:%M:%S"`
 
-cd /home/maxine91/scratch/sorted_align_bam.dir
+cd /home/maxine91/projects/def-jfu/results/divided_minimap.dir
 module load StdEnv/2020 minimap2/2.24 samtools/1.16.1
+
 
 echo "Starting task $SLURM_ARRAY_TASK_ID"
 sample_name=$(sed -n "${SLURM_ARRAY_TASK_ID}p" sample_list)
+echo "Working directory is $(pwd), the sample name is $sample_name"
 
 
 minimap2 -R @RG\\tID:Pacbio2022\\tSM:$sample_name \
@@ -25,36 +27,40 @@ minimap2 -R @RG\\tID:Pacbio2022\\tSM:$sample_name \
     > alndivided.$sample_name.sam
 
 if [ $? -eq 0 ]; then
-  echo "Alignment completes!"
+  echo "Alignment succeed!"
 else
-  echo "alignment is failed."
+  echo "Alignment failed."
+  exit
 fi
 
 samtools view -@ 31 -b alndivided.$sample_name.sam > alndivided.$sample_name.bam
 #Number of BAM compression threads to use in addition to main thread [0].
 if [ $? -eq 0 ]; then
-  echo "Converting completes!"
+  echo "Converting sam to bam succeed!"
 else
-  echo "Converting is failed."
+  echo "Converting sam to bam failed."
+  exit
 fi
 
 samtools sort -m 900M -@ 31 alndivided.$sample_name.bam -o divsorted.$sample_name.bam
 # -m :Approximately the maximum required memory per thread, specified either in bytes or with a K, M, or G suffix. [768 MiB]
 # To prevent sort from creating a huge number of temporary files, it enforces a minimum value of 1M for this setting.
 # -@ INT: Set number of sorting and compression threads. By default, operation is single-threaded.
-echo "Sorting completes!"
+
 if [ $? -eq 0 ]; then
-  echo "Sorting completes!"
+  echo "Sorting succeed!"
 else
-  echo "Sorting is failed."
+  echo "Sorting failed."
+  exit
 fi
 
 samtools index -@ 31 divsorted.$sample_name.bam
-echo "Index completes!"
+
 if [ $? -eq 0 ]; then
-  echo "Index completes!"
+  echo "Indexing succeed!"
 else
-  echo "Index is failed."
+  echo "Indexing failed."
+  exit
 fi
 
 finish_time=`date --date='0 days ago' "+%Y-%m-%d %H:%M:%S"`
